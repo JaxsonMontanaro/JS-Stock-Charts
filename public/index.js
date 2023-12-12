fb21d28157f3fb79ba6da4fdcf13c5a3d3d93b2d
JaxsonMontanaro <jaxsonmontanaro@gmail.com>
2023-12-11 20:52:39 -0800

    changed

--- public/index.js
+++ public/index.js
@@ -1,9 +1,73 @@
+const apiKey = '9c99a3179c5a4eb08e3c382e2562f683';
+
+let GME = result.GME
+let MSFT = result.MSFT
+let DIS = result.DIS
+let BTNX = result.BTNX
+
+const stocks = [GME, MSFT, DIS, BNTX];
+
+// Bonus Note: 
+// Another way to write the above lines would to refactor it as:
+   // const {GME, MSFT, DIS, BTNX} = result 
+// This is an example of "destructuring" an object
+// "Destructuring" creates new variables from an object or an array
+
+
 async function main() {
+
+    // Example endpoint for getting stock quote
+    const endpoint = `https://api.twelvedata.com/time_series?symbol=${stocks}&interval=1d&apikey=${apiKey}`;
+
+    // Make the fetch request
+    fetch(endpoint)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data); // Handle the API response data here
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
 
     const timeChartCanvas = document.querySelector('#time-chart');
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
 
 }
-
+// Call the function to fetch data when the page loads (or when needed)
 main()
+
+
+
+function getColor(stock){
+    if(stock === "GME"){
+        return 'rgba(61, 161, 61, 0.7)'
+    }
+    if(stock === "MSFT"){
+        return 'rgba(209, 4, 25, 0.7)'
+    }
+    if(stock === "DIS"){
+        return 'rgba(18, 4, 209, 0.7)'
+    }
+    if(stock === "BNTX"){
+        return 'rgba(166, 43, 158, 0.7)'
+    }
+}
+
+
+stocks.forEach( stock => stock.values.reverse())
+
+// Time Chart
+new Chart(timeChartCanvas.getContext('2d'), {
+    type: 'line',
+    data: {
+        labels: stocks[0].values.reverse().map(value => value.datetime),
+        datasets: stocks.map(stock => ({
+            label: stock.meta.symbol,
+            data: stock.values.reverse().map(value => parseFloat(value.high)),
+            backgroundColor: getColor(stock.meta.symbol),
+            borderColor: getColor(stock.meta.symbol),
+        }))
+    }
+});
+
